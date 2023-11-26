5893e841d0a06682d5aac1f6fc14562867749a84
AlaMdalla <101179732+AlaMdalla@users.noreply.github.com>
2023-11-26 18:12:32 +0000

    change base url0

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,16 +19,11 @@ app.use(express.json());
 
 app.use(cors({
   credentials: true,
-  origin: ["http://localhost:4200"]
+  origin: ["*"],
 }));
 
-// Add middleware to set CORS headers
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://www.marimex.tn');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+
+
 
 // Log middleware for tracking incoming requests
 app.use((req, res, next) => {
