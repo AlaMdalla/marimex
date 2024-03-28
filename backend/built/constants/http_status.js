7d56ed4ae6547f1acda13435dbe729f7c0e0f3d0
AlaMdalla <101179732+AlaMdalla@users.noreply.github.com>
2024-03-28 13:01:35 +0000

    cors origin changing

--- backend/built/constants/http_status.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HTTP_NOT_FOUND = exports.HTTP_BAD_REQUEST = void 0;
-exports.HTTP_BAD_REQUEST = 400;
-exports.HTTP_NOT_FOUND = 404;
